38c1eef1abddb5cc965c010285ceb5525f9d9469
darth-mama <128187728+darth-mama@users.noreply.github.com>
2024-03-12 05:13:48 -0500

    first commit

---
+++ part_1/app.js
@@ -0,0 +1,25 @@
+let favNum = 777;
+let baseURL = "http://numbersapi.com";
+
+async function num_api() {
+  let data = await $.getJSON(`${baseURL}/${favNumber}?json`);
+  console.log(data);
+}
+num_api();
+
+const favNumbers = [1776, 1492, 22];
+async function mult_num() {
+  let data = await $.getJSON(`${baseURL}/${favNumbers}?json`);
+  console.log(data);
+}
+mult_num();
+
+async function four_facts() {
+  let facts = await Promise.all(
+    Array.from({ length: 4 }, () => $.getJSON(`${baseURL}/${favNum}?json`))
+  );
+  facts.forEach((data) => {
+    $("body").append(`<p>${data.text}</p>`);
+  });
+}
+four_facts();
